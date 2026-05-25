9e5cfcd9ad332a26aac80ab9395f0ee33c5e0933
bloopr11 <thayran.ptaku@gmail.com>
2026-05-25 10:20:31 +0700

    test ml score to 2

--- ml_scorer.py
+++ ml_scorer.py
@@ -24,8 +24,8 @@ import pickle
 import numpy as np
 from typing import Optional, Tuple
 
-ML_MIN_ROWS      = int(os.getenv("ML_MIN_ROWS",      "150"))
-ML_RETRAIN_EVERY = int(os.getenv("ML_RETRAIN_EVERY", "50"))
+ML_MIN_ROWS      = int(os.getenv("ML_MIN_ROWS",      "2"))
+ML_RETRAIN_EVERY = int(os.getenv("ML_RETRAIN_EVERY", "2"))
 MODEL_PATH       = os.getenv("MODEL_PATH", "ml_model.pkl")
 
 # ==================================================
